3364f343422176c91a95e7e60762f58f034b801d
Vatsal <vatsalpatel26003@gmail.com>
2025-07-19 17:47:22 +0530

    add module hot reloading

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,12 +4,19 @@ import {
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
+import livewire from '@defstudio/vite-livewire-plugin';
+
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: false,
         }),
+
+        livewire({
+            refresh: ['resources/css/app.css'],
+        }),
+
         tailwindcss(),
     ],
     server: {
